9078a960e3cb7c255841146fba9ccb863bea31aa
bhdryaman <bhdr.yaman@gmail.com>
2016-10-28 13:24:11 +0300

    configuration with @Resource and @Service...

--- src/_01/collection/test/CollectionInjectionTest.java
+++ src/_01/collection/test/CollectionInjectionTest.java
@@ -3,6 +3,7 @@ package _01.collection.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import _01.model.Department;
 import _01.model.Job;
 
 public class CollectionInjectionTest {
@@ -14,6 +15,10 @@ public class CollectionInjectionTest {
 		Job personBean = context.getBean("job", Job.class);
 		
 		System.out.println(personBean);
+		
+		Department departmentBean = context.getBean("departmentAnnotation", Department.class);
+		
+		System.out.println(departmentBean);
 		
 		((ClassPathXmlApplicationContext) context).close();
 
---
+++ src/_01/model/Department.java
@@ -0,0 +1,45 @@
+package _01.model;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service("departmentAnnotation")
+public class Department {
+	
+	private String Name;
+
+	/*
+	 * Use the annotation @Resource when injecting the collection
+	 * also define the "util:list" in XML file
+	 */
+	@Resource(name="employeeList")
+	private List<String> employeeList;
+	
+	public Department() {
+
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public List<String> getEmployeeList() {
+		return employeeList;
+	}
+
+	public void setEmployeeList(List<String> employeeList) {
+		this.employeeList = employeeList;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [Name=" + Name + ", employeeList=" + employeeList + "]";
+	}
+
+}
