50b06306a9812e9b83fa4144373213974942ad09
bhdryaman <bhdr.yaman@gmail.com>
2016-10-28 18:28:24 +0300

    JSR-330 style...

--- src/_01/service/JobService.java
+++ src/_01/service/JobService.java
@@ -1,14 +1,19 @@
 package _01.service;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import _01.dao.JobDAO;
 
-@Service(value="jobService")
+//@Service(value="jobService")
+@Named(value="jobService")
 public class JobService {
 	
-	@Autowired
+	//@Autowired
+	@Inject
 	private JobDAO jobDao;
 
 	public JobDAO getJobDao() {
